7f8ad443ffb08fbbf1f89c4ee15469f76b862e6b
LC Clark <laura.mc.clark@gmail.com>
2022-11-13 16:58:58 -0500

    adds day 12

---
+++ day-12/script.js
@@ -0,0 +1,15 @@
+const pressed = [];
+const secretCode = 'bizzandwich';
+const secretDiv = document.getElementById('secret-div')
+
+function easterEgg(e) {
+    console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length -1,(pressed.length - secretCode.length));
+    console.log(pressed);
+    if (pressed.join('').includes(secretCode)) {
+secretDiv.innerHTML = "<h1>you win free s'barby's for a year!</h1>";
+    }
+}
+
+window.addEventListener('keyup',easterEgg);
